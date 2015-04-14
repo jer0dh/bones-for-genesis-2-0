d4a279a99b2422f1cf26fb6e1d01b4876dedf2f1
jer0dh <jerod@jhtechservices.com>
2015-04-14 11:38:06 -0500

    Moving one-page website code all to front-page.php

--- front-page.php
+++ front-page.php
@@ -109,3 +109,109 @@ get_footer();
 
 	remove_action( 'genesis_before_content', 'br_genesis_get_sidebar' );
 }*/
+
+
+
+/**
+ * br_site_layout - used to change the order of the sidebar HTML markup in an attempt to use bootstrap grid
+ * Determined though, that the original placement of sidebars was the correct placement for mobile:
+ * ie Secondary sidebar below content and primary sidebar.   Also, primary sidebar below content.
+ * Discovered we could use Bootstrap's push and pull classes to change order in how they are viewed for
+ * desktop
+ *
+ * So this function is not used.  Left for reference.
+ */
+/*function br_site_layout() {
+	if (func_num_args() > 0) {
+		$site_layout = br_get_site_layout(func_get_arg(0));
+	} else {
+		$site_layout = br_get_site_layout();
+	}
+// Secondary Sidebar
+	if ( in_array( $site_layout, array( 'sidebar-content-sidebar', 'sidebar-sidebar-content' ) ) ) {
+		add_action( 'genesis_before_content_sidebar_wrap', 'br_genesis_get_sidebar_alt' );
+	}
+	if ( $site_layout == 'content-sidebar-sidebar' ) {
+		add_action( 'genesis_after_content_sidebar_wrap', 'br_genesis_get_sidebar_alt' );
+	}
+// Primary Sidebar
+	if ( in_array( $site_layout, array( 'sidebar-content-sidebar', 'content-sidebar', 'content-sidebar-sidebar' ) ) ) {
+		add_action( 'genesis_after_content', 'br_genesis_get_sidebar' );
+	}
+	if ( in_array( $site_layout, array( 'sidebar-sidebar-content', 'sidebar-content' ) ) ) {
+		add_action( 'genesis_before_content', 'br_genesis_get_sidebar' );
+	}
+}*/
+
+/**
+ * br_get_site_layout() - used to obtain the layout of a page because genesis_site_layout() would only return
+ * the page's layout if within the loop, but we need the page's layout before the_post() was called in the loop
+ *
+ * If no Post ID is supplied when called, then acts like genesis_site_layout()
+ * Optional Parameter: Integer (Post ID)
+ * @return mixed|string
+ */
+
+function br_get_site_layout() {
+	if (func_num_args() > 0) {
+
+		$custom_layout = get_post_meta( func_get_arg(0), '_genesis_layout', true);
+
+		$site_layout = $custom_layout ? $custom_layout : genesis_get_option( 'site_layout' );
+	} else {
+		$site_layout = genesis_site_layout();
+	}
+
+	return $site_layout;
+}
+
+
+function br_get_sidebar( $name = null ) {
+	/**
+	 * Fires before the sidebar template file is loaded.
+	 *
+	 * Replacing WordPress get_slider so locate_template can be called with Require_once = false
+	 * otherwise only the first section will get the sidebars.
+	 */
+	do_action( 'get_sidebar', $name );
+
+	$templates = array();
+	$name = (string) $name;
+	if ( '' !== $name )
+		$templates[] = "sidebar-{$name}.php";
+
+	$templates[] = 'sidebar.php';
+
+	// Backward compat code will be removed in a future release
+	if ('' == locate_template($templates, true, false))  // added false to change default of requireOnce
+		load_template( ABSPATH . WPINC . '/theme-compat/sidebar.php');
+}
+
+/**
+ * Replacing the genesis_get_sidebar() so to call the br_get_sidebar() instead of the WordPress get_slider()
+ *
+ */
+function br_genesis_get_sidebar() {
+
+	// false says no caching of the site_layout
+	$site_layout = genesis_site_layout(false);
+	//* Don't load sidebar on pages that don't need it
+	if ( 'full-width-content' === $site_layout )
+		return;
+	br_get_sidebar();
+
+}
+/**
+ * Replacing the genesis_get_sidebar_alt() so to call the br_get_sidebar() instead of the WordPress get_slider()
+ *
+ */
+function br_genesis_get_sidebar_alt() {
+
+	$site_layout = genesis_site_layout(false);
+
+	//* Don't load sidebar-alt on pages that don't need it
+	if ( in_array( $site_layout, array( 'content-sidebar', 'sidebar-content', 'full-width-content' ) ) )
+		return;
+	br_get_sidebar( 'alt' );
+
+}
